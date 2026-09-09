8de6d9941868084a322f2c68bcf175a7bcbe3567
Muhammad Taufan <mtaufan@salamsetara.com>
2026-09-10 06:39:59 +0700

    feat: trial classes index + roster endpoints

--- app/controllers/trial_classes_controller.rb
+++ app/controllers/trial_classes_controller.rb
@@ -1,7 +1,31 @@
 class TrialClassesController < ApplicationController
   def index
+    trial_classes = TrialClass.all
+    confirmed_counts = Booking.confirmed.group(:trial_class_id).count
+
+    data = trial_classes.map do |trial_class|
+      confirmed_count = confirmed_counts[trial_class.id] || 0
+      {
+        id: trial_class.id,
+        subject: trial_class.subject,
+        starts_at: trial_class.starts_at,
+        capacity: trial_class.capacity,
+        seats_remaining: trial_class.seats_remaining(confirmed_count)
+      }
+    end
+
+    render json: { data: data }
   end
 
   def roster
+    trial_class = TrialClass.find(params[:id])
+    students = trial_class.bookings.confirmed.includes(:student).map do |booking|
+      {
+        id: booking.student.id,
+        name: booking.student.name
+      }
+    end
+
+    render json: { data: students }
   end
 end
--- app/models/trial_class.rb
+++ app/models/trial_class.rb
@@ -1,3 +1,7 @@
 class TrialClass < ApplicationRecord
   has_many :bookings
+
+  def seats_remaining(confirmed_count = 0)
+    capacity - confirmed_count
+  end
 end
